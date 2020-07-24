8f628c2614407921fd08b556e3da9fbddf5fe779
Tarun Ragiri <tarun.ragiri@peopletech.com>
2020-07-24 20:42:12 +0530

    Updates

---
+++ precache-manifest.0a91e77cf0ff022d3eaa11ed8bfd3a34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d639a3824ead5f5c0577f3bd4b871f01",
+    "url": "/Calculator/index.html"
+  },
+  {
+    "revision": "96a9493a85e1ffb6f339",
+    "url": "/Calculator/static/css/main.7bc1afc8.chunk.css"
+  },
+  {
+    "revision": "c8464c54e2c771519e42",
+    "url": "/Calculator/static/js/2.2b7cfe64.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Calculator/static/js/2.2b7cfe64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96a9493a85e1ffb6f339",
+    "url": "/Calculator/static/js/main.e5285d8a.chunk.js"
+  },
+  {
+    "revision": "8db31b064112002f5bad",
+    "url": "/Calculator/static/js/runtime-main.1d8ddf6c.js"
+  }
+]);
